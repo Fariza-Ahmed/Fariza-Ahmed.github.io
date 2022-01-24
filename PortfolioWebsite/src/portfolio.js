459874fd2abebd123efcc82f7751c4d70689619a
Fariza-Ahmed <92708964+Fariza-Ahmed@users.noreply.github.com>
2022-01-23 17:09:51 -0800

    Adding 'x' in navbar
    Adding 'x' in navbar

--- PortfolioWebsite/src/portfolio.js
+++ PortfolioWebsite/src/portfolio.js
@@ -1,8 +1,12 @@
 const hamburgerButton = document.getElementById('hamburger');
+const xButton = document.getElementById('x');
 const navList = document.getElementById('navList');
 
 function toggleButton() {
     navList.classList.toggle('show');
+    xButton.classList.toggle('show');
+    hamburgerButton.classList.toggle('show');
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+xButton.addEventListener('click', toggleButton);
